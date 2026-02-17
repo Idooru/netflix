bc7d6eacfaafc8d6d31ee51a281774f3c5869db1
shlee <shere1765@gmail.com>
2026-02-17 14:30:04 +0900

    ValidationPipe 주요 옵션 알아보기

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -29,11 +29,85 @@ import {
   MinLength,
   NotContains,
   NotEquals,
+  registerDecorator,
+  Validate,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
 } from 'class-validator';
 
 enum MovieGenre {
   Fantasy = 'fantasy',
   Action = 'action',
+}
+
+@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(
+    value: string,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    if (!value) {
+      return false;
+    }
+    return value.length > 4 && value.length < 8;
+  }
+  defaultMessage?(arg?: ValidationArguments): string {
+    return `비밀번호의 길이는 4 ~ 8자 여야 합니다. 입력된 비밀번호 (${arg?.value})`;
+  }
+}
+
+function IsPasswordValid(options?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options,
+      validator: PasswordValidator,
+    });
+  };
+}
+
+@ValidatorConstraint()
+class EmailValidator implements ValidatorConstraintInterface {
+  validate(
+    value: string,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    if (!value) {
+      return false;
+    }
+
+    if (!(value.length > 12 && value.length < 25)) {
+      return false;
+    }
+
+    if (!value.includes('@')) {
+      return false;
+    }
+
+    return true;
+  }
+
+  defaultMessage?(args?: ValidationArguments): string {
+    if (!args?.value) {
+      return '이메일을 입력하지 않았습니다.';
+    }
+
+    return `이메일 형식에 어긋납니다. (${args?.value}) `;
+  }
+}
+
+function IsEmailValid(options?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options,
+      validator: EmailValidator,
+    });
+  };
 }
 
 export class UpdateMovieDto {
@@ -76,6 +150,10 @@ export class UpdateMovieDto {
   // @MinLength(5)
   // @IsUUID()
   // @IsLatLong()
+  // test: string;
+  @IsPasswordValid()
+  password: string;
 
-  test: string;
+  @IsEmailValid()
+  email: string;
 }
