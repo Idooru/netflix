c2606db7c0530e2f0a4ff7ffa69391e121bd1f54
shlee <shere1765@gmail.com>
2026-03-07 17:55:07 +0900

    Mapped Types 적용하기

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,27 +1,4 @@
-import { ArrayNotEmpty, IsArray, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { CreateMovieDto } from './create-movie.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-enum MovieGenre {
-  Fantasy = 'fantasy',
-  Action = 'action',
-}
-
-export class UpdateMovieDto {
-  @IsNotEmpty()
-  @IsOptional()
-  @IsString()
-  title?: string;
-
-  @IsOptional()
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsNumber({}, { each: true })
-  genreIds?: number[];
-
-  @IsNotEmpty()
-  @IsOptional()
-  detail?: string;
-
-  @IsNotEmpty()
-  @IsOptional()
-  directorId?: number;
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
